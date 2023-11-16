551d923156f0aa746f8d35bc4aaf4f3d4623e082
Niels Hoppe <mail@nielshoppe.de>
2023-11-16 07:50:41 +0100

    Various updates

--- make_slides.sh
+++ make_slides.sh
@@ -1,1 +1,6 @@
+#cd slides
+#NB_PATH="$1"
+#REL_NB=${NB_PATH/#slides\//}
+#jupyter nbconvert --to slides $REL_NB --output-dir ../docs/
+
 jupyter nbconvert slides/*.ipynb --to slides --output-dir docs/
